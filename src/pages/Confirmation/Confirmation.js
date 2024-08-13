9fb5804af3edc3a2713680d7a47bc50c5203b13d
mlgrzt <goskolarska@gmail.com>
2024-08-13 17:33:33 +0200

    updated Confirmation page

--- src/pages/Confirmation/Confirmation.js
+++ src/pages/Confirmation/Confirmation.js
@@ -1,22 +1,33 @@
 import React from "react";
 import Hero from "../../components/Hero";
+import styles from "./Confirmation.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleCheck } from "@fortawesome/free-regular-svg-icons";
 
 const Confirmation = () => {
   return (
-    <div>
+    <div className={styles.confirmation}>
       <Hero title="Potwierdzenie" />
       <div className="container">
-        <div>
-          <img />
-          <div>
-            <h2>Dziękujemy za potwierdzenie uczestnictwa!</h2>
-            <p>
+        <div className={styles.confirmationContent}>
+          <FontAwesomeIcon
+            icon={faCircleCheck}
+            className={styles.confirmationIcon}
+          />
+
+          <div className={styles.confirmationText}>
+            <h2 className={styles.confirmationTitle}>
+              Dziękujemy za potwierdzenie uczestnictwa!
+            </h2>
+            <p className={styles.confirmationDescription}>
               Pomaga nam to utrzymać aktywność wśród członków, następne
               powiadomienie otrzymasz za 60 dni.
             </p>
           </div>
         </div>
-        <a>Wroc na portal</a>
+        <a href="/" className={styles.confirmationButton}>
+          Wróć na portal
+        </a>
       </div>
     </div>
   );
