0371a0909c797cd93c0ece6d587fae781298b409
mlgrzt <goskolarska@gmail.com>
2024-08-22 23:21:34 +0200

    updated panel expanded

--- src/pages/Panel/PanelHome/PanelHome.js
+++ src/pages/Panel/PanelHome/PanelHome.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheck, faPlus } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCheck,
+  faBell,
+  faGear,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
 import Hero from "../../../components/Hero";
 import GroupsSection from "./GroupsSection";
 import styles from "./PanelHome.module.css";
@@ -51,75 +56,79 @@ const PanelHome = () => {
       <div className="container">
         {selectedGroup ? (
           <div className={styles.expandedView}>
-            <div className={styles.groupDetail}>
-              <img
-                src={selectedGroup.imgSrc}
-                alt={selectedGroup.alt}
-                className={styles.image}
-              />
-              <h3 className={styles.cardTitle}>{selectedGroup.title}</h3>
-              <button className={styles.cardButton}>
-                <FontAwesomeIcon icon={faCheck} />
-                Potwierdź uczestnictwo
-              </button>
-              <p className={styles.cardDetailInfo}>
-                Uczestnictwo odnawia się co 60 dni
-              </p>
-              <div className={styles.cardDetailConfirmationNumber}>
-                <div className={styles.cardDetailConfirmationNumberCounter}>
-                  <FontAwesomeIcon
-                    icon={faCheck}
-                    lassName={styles.cardDetailConfirmationCheckIcon}
-                  />
-                  <span>14</span>z<span>20</span>
-                </div>
-                <div className={styles.cardDetailConfirmationNumberTitle}>
-                  członków potwierdziło modlitwę
-                </div>
-              </div>
-              <div className={styles.intentionSection}>
-                <h4 className={styles.intentionTitle}>Intencja na Maj 2024</h4>
-                <p className={styles.intentionDescription}>
-                  O docenienie daru różnorodności w Kościele
+            <div className={styles.expandedViewGroupDetail}>
+              <h2 className={styles.expandedViewGroupDetailTitle}>
+                Aktywne grupy
+              </h2>
+              <div className={styles.groupDetail}>
+                <h3 className={styles.cardTitle}>{selectedGroup.title}</h3>
+                <button className={styles.cardButton}>
+                  <FontAwesomeIcon icon={faCheck} />
+                  Potwierdź uczestnictwo
+                </button>
+                <p className={styles.cardDetailInfo}>
+                  Uczestnictwo odnawia się co 60 dni
                 </p>
-                <p className={styles.intentionText}>
-                  Módlmy się do Ducha Świętego, aby pomógł nam rozeznać dary
-                  różnych charyzmatów we wspólnotach chrześcijańskich i odkryć
-                  bogactwo wielu tradycji liturgicznych wewnątrz Kościoła.
-                </p>
-              </div>
-              <div className={styles.prayerList}>
-                <h3 className={styles.prayerListTitle}>Modlitwa za:</h3>
-                <div className={styles.prayerNames}>
-                  <span>José</span>
-                  <span>Bruno</span>
-                  <span>Luiza</span>
-                  <span>Lara</span>
-                  <span>Laura</span>
-                  <span>Camila</span>
-                  <span>Camila</span>
-                  <span>Thiago</span>
-                  <span>Vinícius</span>
-                  <span>Pedro</span>
-                  <span>Felipe</span>
-                  <span>Eduarda</span>
-                  <span>Sofia</span>
-                  <span>Maria</span>
-                  <span>Rafael</span>
-                  <span>José</span>
-                  <span>Bruno</span>
-                  <span>Luiza</span>
-                  <span>Lara</span>
-                  <span>Laura</span>
+                <div className={styles.cardDetailConfirmationNumber}>
+                  <div className={styles.cardDetailConfirmationNumberCounter}>
+                    <FontAwesomeIcon
+                      icon={faCheck}
+                      lassName={styles.cardDetailConfirmationCheckIcon}
+                    />
+                    <span>14</span>z<span>20</span>
+                  </div>
+                  <div className={styles.cardDetailConfirmationNumberTitle}>
+                    członków potwierdziło modlitwę
+                  </div>
                 </div>
-              </div>
-              <div className={styles.additionalSettings}>
-                <a href="#" className={styles.settingsLink}>
-                  Ustawienia powiadomień
-                </a>
-                <a href="#" className={styles.settingsLink}>
-                  Dodatkowe ustawienia
-                </a>
+                <div className={styles.intentionSection}>
+                  <h4 className={styles.intentionTitle}>
+                    Intencja na Maj 2024
+                  </h4>
+                  <p className={styles.intentionDescription}>
+                    O docenienie daru różnorodności w Kościele
+                  </p>
+                  <p className={styles.intentionText}>
+                    Módlmy się do Ducha Świętego, aby pomógł nam rozeznać dary
+                    różnych charyzmatów we wspólnotach chrześcijańskich i odkryć
+                    bogactwo wielu tradycji liturgicznych wewnątrz Kościoła.
+                  </p>
+                </div>
+                <div className={styles.prayerList}>
+                  <h3 className={styles.prayerListTitle}>Modlitwa za:</h3>
+                  <div className={styles.prayerNames}>
+                    <span>José</span>
+                    <span>Bruno</span>
+                    <span>Luiza</span>
+                    <span>Lara</span>
+                    <span>Laura</span>
+                    <span>Camila</span>
+                    <span>Camila</span>
+                    <span>Thiago</span>
+                    <span>Vinícius</span>
+                    <span>Pedro</span>
+                    <span>Felipe</span>
+                    <span>Eduarda</span>
+                    <span>Sofia</span>
+                    <span>Maria</span>
+                    <span>Rafael</span>
+                    <span>José</span>
+                    <span>Bruno</span>
+                    <span>Luiza</span>
+                    <span>Lara</span>
+                    <span>Laura</span>
+                  </div>
+                </div>
+                <div className={styles.additionalSettings}>
+                  <a href="#" className={styles.settingsLink}>
+                    <FontAwesomeIcon icon={faBell} />
+                    Ustawienia powiadomień
+                  </a>
+                  <a href="#" className={styles.settingsLink}>
+                    <FontAwesomeIcon icon={faGear} />
+                    Dodatkowe ustawienia
+                  </a>
+                </div>
               </div>
             </div>
             <GroupsSection
